4593a776d438295987b5d9f717f3b057d348f950
Andrew <Andrew@192.168.0.4>
2019-03-25 21:05:52 -0700

    3

--- edcast/src/test/java/com/edcast/ExtentReportListener/ExtentReportListener.java
+++ edcast/src/test/java/com/edcast/ExtentReportListener/ExtentReportListener.java
@@ -24,7 +24,7 @@ import com.relevantcodes.extentreports.LogStatus;
 		@Override
 		public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites,
 				String outputDirectory) {
-			extent = new ExtentReports("\reports" + File.separator
+			extent = new ExtentReports("\\reports" + File.separator
 					+ "Extent.html", true);
 			
 
